b6f49eb2ec72eddc25360fc3902cd04f4d64adda
varunngoyal <varungoyal311213@gmail.com>
2019-08-10 19:15:30 +0530

    1.Smoke Test

---
+++ contracts/Election.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.5.8;
+
+contract Election {
+	//Store candidate
+	//Read candidate
+
+	string public candidate; 
+	// Constructor
+	constructor () public {
+		candidate = "Candidate 1";
+	}
+
+}
