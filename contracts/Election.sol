f7baa16077ce2461d1b903a0a83c0f09177ff8d9
varunngoyal <varungoyal311213@gmail.com>
2019-08-12 13:27:03 +0530

    4. Add events

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -22,6 +22,11 @@ contract Election {
 		candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
 	}
 
+	//voted event
+	event votedEvent (
+		uint indexed _candidateId
+	);
+
 	constructor () public {
 		addCandidate("Candidate 1");
 		addCandidate("Candidate 2");
@@ -39,6 +44,9 @@ contract Election {
 
 		//update candidate vote Count
 		candidates[_candidateId].voteCount ++;
+
+		//trigger voted event
+		emit votedEvent(_candidateId);
 	}
 
 
--- test/election.js
+++ test/election.js
@@ -33,6 +33,9 @@ contract("Election", function(accounts) {
       candidateId = 1;
       return electionInstance.vote(candidateId, { from: accounts[0] });
     }).then(function(receipt) {
+      assert.equal(receipt.logs.length, 1, "an event was triggered");
+      assert.equal(receipt.logs[0].event, "votedEvent", "the event type is correct");
+      assert.equal(receipt.logs[0].args._candidateId.toNumber(), candidateId, "the candidate id is correct");
       return electionInstance.voters(accounts[0]);
     }).then(function(voted) {
       assert(voted, "the voter was marked as voted");
@@ -42,6 +45,7 @@ contract("Election", function(accounts) {
       assert.equal(voteCount, 1, "increments the candidate's vote count");
     })
   });
+
 
   it("throws an exception for invalid candidates", function() {
     return Election.deployed().then(function(instance) {
