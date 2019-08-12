37f18850e5bcddb6fc2a4bd9b408015d69ac371a
varunngoyal <varungoyal311213@gmail.com>
2019-08-12 10:36:55 +0530

    2. List Candidates

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,13 +1,27 @@
 pragma solidity ^0.5.8;
 
 contract Election {
-	//Store candidate
-	//Read candidate
-
-	string public candidate; 
-	// Constructor
-	constructor () public {
-		candidate = "Candidate 1";
+	// Model a Candidate
+	struct Candidate {
+		uint id;
+		string name;
+		uint voteCount;
 	}
+	// Store Candidates
+	// Fetch Candidate
+	mapping(uint => Candidate) public candidates;
+	// Store Candidates Count
+	uint public candidatesCount;
+
+	function addCandidate (string memory _name) private {
+		candidatesCount ++;
+		candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+	}
+
+	constructor () public {
+		addCandidate("Candidate 1");
+		addCandidate("Candidate 2");
+	}
+
 
 }
